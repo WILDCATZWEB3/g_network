e0859d502a81ffd35fccb46934ae05b99335a764
WILDCATZWEB3 <dydxcryptocoin@gmail.com>
2025-02-11 16:14:43 +0100

    Updated homepage

---
+++ ap.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div style={{ textAlign: 'center', padding: '50px' }}>
+            <h1>🚀 Welcome to G Network!</h1>
+            <p>The Web3 learning future starts here.</p>
+        </div>
+    );
+}
